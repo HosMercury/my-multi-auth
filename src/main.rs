a50dd311e157d53708c799a4cf609ac82024c0c8
HosMercury <hos.mercury@gmail.com>
2024-01-30 01:41:08 +0200

    initial

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::{env, sync::Arc};
+
+use axum::{routing::get, Router};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use time::Duration;
+use tower_sessions::{Expiry, SessionManagerLayer};
+use tower_sessions_redis_store::{fred::prelude::*, RedisStore};
+
+struct AppState {
+    app_name: &'static str,
+    pool: Pool<Postgres>,
+}
+
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().expect("error loading .env");
+
+    ////////////////// SESSION //////////////////////////////
+    let pool = RedisPool::new(RedisConfig::default(), None, None, None, 6).unwrap();
+    pool.connect();
+    pool.wait_for_connect().await.unwrap();
+    let session_store = RedisStore::new(pool);
+    let session_layer = SessionManagerLayer::new(session_store)
+        .with_secure(false)
+        .with_expiry(Expiry::OnInactivity(Duration::seconds(10)));
+    ////////////////// DB //////////////////////////////
+    let db_url: String = env::var("DATABASE_URL").unwrap();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await
+        .expect("error connection to db");
+    /////////////////////////////////// AXUM ////////////////////////////
+    let shared_state = Arc::new(AppState {
+        app_name: "Multi Auth",
+        pool,
+    });
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .layer(session_layer)
+        .with_state(shared_state);
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
